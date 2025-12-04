d9d04f1fc65664a27debbcf38b55649ab64777fd
msimmons1535-d <tnmglxf047@hotmail.com>
2025-12-05 03:35:03 +0800

    add config.yml, index.ts, EthereumConfig.ts

---
+++ dist-provider-api.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default function import() { return <div>Component</div>; }
---
+++ factory-local-global.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\factory-local-global.py
+// Generated at: 2025-10-19T09:05:31.913Z
